292bd0f06956a27401c2841e94cb3187482a8345
Keqi Huang <keqih@smu.edu>
2019-02-26 23:42:16 +0800

    Create A.py

---
+++ Codeforces/Codeforces Round #542 [Alex Lopashev Thanks-Round] (Div. 2)/A.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = [int(i) for i in input().split(' ')]
+
+def helper(a):
+    pos, neg = 0, 0
+    for i in a:
+        if i > 0:
+            pos += 1
+        elif i < 0:
+            neg += 1
+    if pos >= n / 2:
+        return 1
+    elif neg >= n / 2:
+        return -1
+    else:
+        return 0
+
+print(helper(a))
