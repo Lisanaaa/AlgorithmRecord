d9d5fe45efa34a1c5df86a2b8eab7cb8c991f9f3
Keqi Huang <keqih@smu.edu>
2019-02-27 09:10:41 +0800

    Update B.py

--- Codeforces/Codeforces Round #542 [Alex Lopashev Thanks-Round] (Div. 2)/B.py
+++ Codeforces/Codeforces Round #542 [Alex Lopashev Thanks-Round] (Div. 2)/B.py
@@ -1,18 +1,5 @@
 “”“
 每一轮的选择跟下一轮都没有关系
-
-Let 𝑥𝑖 and 𝑦𝑖 be the index of the cakes with size 𝑖.
-
-We know that if either Sasha or Dima walks from 𝑥𝑖 to 𝑥𝑖+1 
-then the other one will have no choice but to walk from 𝑦𝑖 to 𝑦𝑖+1. 
-Similarly, if one walks from 𝑥𝑖 to 𝑦𝑖+1 then the other one will have to walk from 𝑦𝑖 to 𝑥𝑖+1.
-
-Therefore, 𝐶𝑖 the total distance of moving from cake of size 𝑖 to 𝑖+1 is 𝑚𝑖𝑛(|𝑥𝑖−𝑥𝑖+1|+|𝑦𝑖−𝑦𝑖+1|,|𝑥𝑖−𝑦𝑖+1|+|𝑦𝑖−𝑥𝑖+1|).
-
-The answer is ∑𝑖=1𝑛−1𝐶𝑖.
-
-The time complexity is (𝑛).
-
 ”“”
 
 
