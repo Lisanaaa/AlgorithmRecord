482c2094442317a6dff94fedd55b803628126ded
Keqi Huang <keqih@smu.edu>
2019-03-20 18:27:54 +0800

    Update A.go

--- Codeforces/Codeforces Round #542 [Alex Lopashev Thanks-Round] (Div. 2)/A.go
+++ Codeforces/Codeforces Round #542 [Alex Lopashev Thanks-Round] (Div. 2)/A.go
@@ -8,13 +8,22 @@ import (
 	"strings"
 )
 
-func cleanString(stream string, seperator string) []int{
+func getNumFromOneLine(ir *bufio.Reader) int {
+	stream, _ := ir.ReadString('\n')
 	// Trims the stream and then splits
 	trimmedStream := strings.TrimSpace(stream)
-	splitArr := strings.Split(trimmedStream, seperator)
+	num, _ := strconv.Atoi(trimmedStream)
+	return num
+}
+
+func getNumsFromOneLine(ir *bufio.Reader) []int {
+	stream, _ := ir.ReadString('\n')
+	// Trims the stream and then splits
+	trimmedStream := strings.TrimSpace(stream)
+	splitArr := strings.Split(trimmedStream, " ")
 	// convert strings to integers and store them in a slice
 	nums := make([]int, len(splitArr))
-	for index,val := range splitArr{
+	for index, val := range splitArr {
 		nums[index], _ = strconv.Atoi(val)
 	}
 	return nums
@@ -22,11 +31,9 @@ func cleanString(stream string, seperator string) []int{
 
 func main() {
 	ir := bufio.NewReader(os.Stdin)
-	input, _ := ir.ReadString('\n')
-	_, _ = strconv.Atoi(strings.TrimSpace(input))
-	var nums []int
-	lineInput, _ := ir.ReadString('\n')
-	nums = cleanString(lineInput, " ")
+	_ = getNumFromOneLine(ir)
+	nums := getNumsFromOneLine(ir)
+
 	pos, neg := 0, 0
 	for _, num := range nums {
 		if num > 0 {
@@ -35,9 +42,10 @@ func main() {
 			neg += 1
 		}
 	}
-	if neg >= (len(nums) + 1) / 2 {
+
+	if neg >= (len(nums)+1)/2 {
 		fmt.Println(-1)
-	} else if pos >= (len(nums) + 1) / 2 {
+	} else if pos >= (len(nums)+1)/2 {
 		fmt.Println(1)
 	} else {
 		fmt.Println(0)
